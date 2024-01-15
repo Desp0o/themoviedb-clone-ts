c67edacf676d33434b563ac5cb52d9b362ae1167
Despo <tornike.despotashvili@gmail.com>
2024-01-15 17:34:47 +0400

    make card complete / trending movie list div with scroll

--- src/components/movieCard/MovieCard.tsx
+++ src/components/movieCard/MovieCard.tsx
@@ -4,59 +4,71 @@ import "./movieCard.css";
 interface movieCardProps {
   image: string;
   rating: number;
-  title: string,
-  date: string
+  title: string;
+  date: string;
 }
 
-const MovieCard: React.FC<movieCardProps> = ({ image, rating, title, date }) => {
+const MovieCard: React.FC<movieCardProps> = ({
+  image,
+  rating,
+  title,
+  date,
+}) => {
   const dasharray = Math.PI * 100;
   const dashOffset = Math.PI * (100 - rating);
 
-  const [ratingColor, setRatingColor] = useState<string>('')
+  const [ratingColor, setRatingColor] = useState<string>("");
 
-  useEffect(()=>{
-    if( rating >= 70 ){
-      setRatingColor('#5FCF7A')
-    }else if(rating <= 69 && rating >= 50 ){
-      setRatingColor('#D2D643')
-    }else if(rating <= 49 && rating >= 1){
-      setRatingColor('#DC3861')
-    }else if(rating === 0){
-      setRatingColor('#666666')
+  useEffect(() => {
+    if (rating >= 70) {
+      setRatingColor("#5FCF7A");
+    } else if (rating <= 69 && rating >= 50) {
+      setRatingColor("#D2D643");
+    } else if (rating <= 49 && rating >= 1) {
+      setRatingColor("#DC3861");
+    } else if (rating === 0) {
+      setRatingColor("#666666");
     }
-  },[rating])
+  }, [rating]);
 
   return (
-    <div className="movie_Card">
-      <img src={image} alt="movie card" />
+    <div className="movie_card_container">
+      <div className="movie_Card">
+        <img src={image} alt="movie card" />
 
-      <div className="movie_card_rating">
-        <svg width={34} height={34} viewBox="0 0 100 100" className="rating_svg">
-        <circle
-            cx={52.5}
-            cy={52.5}
-            r={50}
-            fill="transparent"
-            stroke={ratingColor}
-            strokeWidth={6}
-            className="full_circle_rating"
-            strokeDasharray={dasharray}
-          />
+        <div className="movie_card_rating">
+          <svg
+            width={34}
+            height={34}
+            viewBox="0 0 100 100"
+            className="rating_svg"
+          >
+            <circle
+              cx={52.5}
+              cy={52.5}
+              r={50}
+              fill="transparent"
+              stroke={ratingColor}
+              strokeWidth={6}
+              className="full_circle_rating"
+              strokeDasharray={dasharray}
+            />
 
-          <circle
-            cx={52.5}
-            cy={52.5}
-            r={50}
-            fill="transparent"
-            stroke={ratingColor}
-            strokeWidth={6}
-            className="full_circle_rating_front"
-            strokeDasharray={dasharray}
-            strokeDashoffset={dashOffset}
-          />
-        </svg>
+            <circle
+              cx={52.5}
+              cy={52.5}
+              r={50}
+              fill="transparent"
+              stroke={ratingColor}
+              strokeWidth={6}
+              className="full_circle_rating_front"
+              strokeDasharray={dasharray}
+              strokeDashoffset={dashOffset}
+            />
+          </svg>
 
-        <p className="movie_card_rating_txt">{rating}</p>
+          <p className="movie_card_rating_txt">{rating}</p>
+        </div>
       </div>
 
       <p className="movie_card_title">{title}</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import MovieCard from "../components/movieCard/MovieCard"
-import testImage from "../assets/images/test.webp"
-import TrendingMovies from "../TrendingMovie"
+
+import TrendingMovies from "../query/trendingMovies/TrendingMovie"
 
 export default function Home() {
   return (
     <div>
-      <MovieCard image={testImage} rating={10} title="" date=""/>
       <TrendingMovies />
     </div>
     
