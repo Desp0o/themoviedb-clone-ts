22f0b93d70f3223af4af8826fd48318435d23e5a
Despo <tornike.despotashvili@gmail.com>
2024-01-15 14:46:12 +0400

    i axios and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 import Home from "./pages/Home";
 import Navbar from "./components/navbar/navbar";
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
+    </QueryClientProvider>
   );
 }
 
