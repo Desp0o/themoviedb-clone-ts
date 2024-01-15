2a8983de4ece03a71179957de96a5412abc4da41
Despo <tornike.despotashvili@gmail.com>
2024-01-15 16:04:03 +0400

    make api call

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import MovieCard from "../components/movieCard/MovieCard"
 import testImage from "../assets/images/test.webp"
+import TrendingMovies from "../TrendingMovie"
 
 export default function Home() {
   return (
     <div>
       <MovieCard image={testImage} rating={10} />
+      <TrendingMovies />
     </div>
     
   )
