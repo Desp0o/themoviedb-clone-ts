f36332eebbea415bc063c679af3c55e3025af831
Despo <tornike.despotashvili@gmail.com>
2024-01-24 14:10:09 +0400

    update fetching

--- src/components/loadingComponent/LoadingComponent.tsx
+++
@@ -1,7 +0,0 @@
-import "./LoadingComponent.css"
-
-export default function LoadingComponent() {
-  return (
-    <div className="LoadingComponent">LoadingComponent</div>
-  )
-}
--- src/pages/singleMoviePage/SingleMoviePage.tsx
+++ src/pages/singleMoviePage/SingleMoviePage.tsx
@@ -3,62 +3,69 @@ import "./SingleMoviePage.css";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import SingleMovieTitle from "../../components/singleMovieComponents/SingleMovieTitle";
-import LoadingComponent from "../../components/loadingComponent/LoadingComponent";
 import RatingCircle from "../../components/movieCard/RatingCircle";
 import TagLine from "../../components/singleMovieComponents/TagLine";
 import SingleMovieOverView from "../../components/singleMovieComponents/SingleMovieOverView";
 import BookMarkLikeSave from "../../components/singleMovieComponents/BookMarkLikeSave";
 import CompanyComponent from "../../components/singleMovieComponents/CompanyComponent";
 import ProductionCountrys from "../../components/singleMovieComponents/ProductionCountrys";
+import loadingImagePattern from "../../assets/images/glyphicons-picture.svg";
 
 const apiKey = import.meta.env.VITE_API_KEY2;
 export default function SingleMoviePage() {
   const { id } = useParams();
-  
 
-  const { isLoading, data } = useQuery("single-movie", () => {
-    return axios.get(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`,
-      {
-        headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-          accept: "application/json",
-        },
-      }
-    );
-  },
-  
-);
+  const { isLoading, data } = useQuery(
+    ["single-movie", id],
+    () => {
+      return axios.get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`,
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+            accept: "application/json",
+          },
+        }
+      );
+    },
+    {
+      cacheTime: 1000, // Set cacheTime to 1 second (1000 milliseconds)
+    }
+  );
 
   const roundedRating = Math.round(data?.data.vote_average * 10);
 
   return (
     <>
-      {isLoading ? (
-        <LoadingComponent />
-      ) : (
-        <div className="SingleMoviePage">
-          <div
-            className="SingleMoviePage_backdrop"
-            style={{
-              backgroundImage: `url(${`http://www.themoviedb.org/t/p/w1920_and_h800_bestv2${data?.data.backdrop_path}`})`,
-            }}
-          >
-            <div className="SingleMoviePage_backdrop_overlay" />
+      <div className="SingleMoviePage">
+        <div
+          className="SingleMoviePage_backdrop"
+          style={{
+            backgroundImage: `url(${
+              isLoading
+                ? loadingImagePattern
+                : `http://www.themoviedb.org/t/p/w1920_and_h800_bestv2${data?.data.backdrop_path}`
+            })`,
+          }}
+        >
+          <div className="SingleMoviePage_backdrop_overlay" />
 
-            <div className="backdrop_inner">
+          <div className="backdrop_inner">
+            <div className="backdrop_inner_poster_container">
+              <img
+                src={`${
+                  isLoading
+                    ? loadingImagePattern
+                    : `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data?.data.poster_path}`
+                }`}
+                className="single_poster"
+                alt="single_poster"
+                style={{ backgroundImage: `${ProductionCountrys}` }}
+              />
+            </div>
 
-              <div className="backdrop_inner_poster_container">
-                <img
-                  src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data?.data.poster_path}`}
-                  className="single_poster"
-                  alt="single_poster"
-                  style={{backgroundImage : `${ProductionCountrys}`}}
-                />
-              </div>
-              
-
-              <div className="backdrop_inner_right_side">
+            <div className="backdrop_inner_right_side">
+              {!isLoading && (
                 <SingleMovieTitle
                   title={data?.data.title}
                   date={data?.data.release_date}
@@ -66,29 +73,39 @@ export default function SingleMoviePage() {
                   country={data?.data.production_countries}
                   runtime={data?.data.runtime}
                 />
+              )}
 
-                <div className="single_movie_page_rating_container">
-                  <RatingCircle
-                    width={68}
-                    height={68}
-                    CircleHeight={64}
-                    CircleWidth={64}
-                    rating={roundedRating}
-                  />
+              <div className="single_movie_page_rating_container">
+                {!isLoading && (
+                  <>
+                    <RatingCircle
+                      width={68}
+                      height={68}
+                      CircleHeight={64}
+                      CircleWidth={64}
+                      rating={roundedRating}
+                    />
 
-                  <BookMarkLikeSave />
-                </div>
-
-                <TagLine tagline={data?.data.tagline} />
-
-                <SingleMovieOverView overview={data?.data.overview} />
+                    <BookMarkLikeSave />
+                  </>
+                )}
               </div>
+
+              {!isLoading && <TagLine tagline={data?.data.tagline} />}
+
+              {!isLoading && (
+                <SingleMovieOverView overview={data?.data.overview} />
+              )}
             </div>
           </div>
-          <CompanyComponent companyList={data?.data.production_companies} />
-          <ProductionCountrys countryList={data?.data.production_countries} />
         </div>
-      )}
+        {!isLoading && (
+          <>
+            <CompanyComponent companyList={data?.data.production_companies} />
+            <ProductionCountrys countryList={data?.data.production_countries} />
+          </>
+        )}
+      </div>
     </>
   );
 }
