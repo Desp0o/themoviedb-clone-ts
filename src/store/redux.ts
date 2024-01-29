70ffc3297cf64718a52b8c0ec46f8b98010929a2
Despo <tornike.despotashvili@gmail.com>
2024-01-29 20:15:49 +0400

    popular list ts fix

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -1,10 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import optionSlice from "./sortingValues"
 
-interface RootState {
-  chooseOption: string; // Adjust the type according to your actual state structure
-}
-
 const store = configureStore({
   reducer: {
     chooseOption: optionSlice,
