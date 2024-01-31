ff6966e2453de4322fb2e9403bcf1fb17ebd9ffb
Despo <tornike.despotashvili@gmail.com>
2024-01-31 16:06:40 +0400

    add vote ranges styles

--- src/components/PopularComponents/averageVoteRange.tsx
+++ src/components/PopularComponents/averageVoteRange.tsx
@@ -1,21 +1,25 @@
-import { useEffect } from "react";
-import rangeBar from "../../assets/icons/rangeBar.gif"
-
 const VoteRange = () => {
-    const bars = ['|', '|', '|', '|', '|', '|', '|', '|', '|', '|',];
-    
+  const bars = "|".repeat(10);
 
-    return(
-        <div className="range_for_vote_inner">
-            <h4 style={{marginBottom:"10px"}}>User Score</h4>
+  return (
+    <div className="range_for_vote_inner">
+      <h4 style={{ marginBottom: "10px" }}>User Score</h4>
 
-            <div className="vote_ragne_container">
-                <div className="bars">{bars && bars.map((bar) => {return <p>{bar} </p>} )}</div>
-            <input type="range" className="vote_range_class"/>
-            </div>
-           
+      <div className="vote_ragne_container">
+        <div className="bars">
+          {bars.split("").map((bar, index) => {
+            return <p key={index}>{bar} </p>;
+          })}
         </div>
-    )
-}
+        <input type="range" className="vote_range_class" step={0} min={1} max={10}/>
+        <div className="vote_numbers">
+        <span className="vote_number vote_zero">0</span>
+        <span className="vote_number vote_five">5</span>
+        <span className="vote_number vote_ten">10</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default VoteRange
+export default VoteRange;
