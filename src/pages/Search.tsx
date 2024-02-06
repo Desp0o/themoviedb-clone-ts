d454efaabd8461d29e0877cad91ae706f66ac313
Despo <tornike.despotashvili@gmail.com>
2024-02-06 14:07:15 +0400

    update search number and lenght compoonent styles

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -11,14 +11,16 @@ interface RootState {
   searchOptions: {
     movieLength: number;
     tvShwowLength: number;
-    scrollTopSearchPage: boolean
+    scrollTopSearchPage: boolean;
   };
 }
 
 const Search = () => {
   const { name } = useParams();
 
-  const [isMovieList, setIsMovieList] = useState(localStorage.getItem('isMovieList') || true);
+  const [isMovieList, setIsMovieList] = useState(
+    localStorage.getItem("isMovieList") || true
+  );
 
   const movieQuery = `https://api.themoviedb.org/3/search/movie?query=${name}&include_adult=false&page=`;
 
@@ -37,77 +39,79 @@ const Search = () => {
 
   const showMovie = () => {
     setIsMovieList(true);
-    localStorage.setItem('isMovieList', 'true')
+    localStorage.setItem("isMovieList", "true");
   };
 
   const showTvSHows = () => {
     setIsMovieList(false);
-    localStorage.setItem('isMovieList', 'false')
+    localStorage.setItem("isMovieList", "false");
   };
 
-  useEffect(()=>{
-    
-    if(localStorage.getItem('isMovieList') === 'true'){
-      setIsMovieList(true)
-    }else if(localStorage.getItem('isMovieList') === 'false'){
-      setIsMovieList(false)
+  useEffect(() => {
+    if (localStorage.getItem("isMovieList") === "true") {
+      setIsMovieList(true);
+    } else if (localStorage.getItem("isMovieList") === "false") {
+      setIsMovieList(false);
     }
-    
-  },[isMovieList])
+  }, [isMovieList]);
 
-
-  const [foo, setFoo] = useState(false)
-  useEffect(()=>{
-    console.log(scrollTopSearchPage + 'scroll');
-    
-  },[foo])
+  const [foo, setFoo] = useState(false);
+  useEffect(() => {
+    console.log(scrollTopSearchPage + "scroll");
+  }, [foo]);
 
   return (
     <>
-    <ScrollToTop />
-    <div className="search">
-      <div className="search_inner">
-        <div className="search_left_block">
-          <div className="search_result">
-            <h3 onClick={()=>setFoo(true)}>Search Results</h3>
+      <ScrollToTop />
+      <div className="search">
+        <div className="search_inner">
+          <div className="search_left_block">
+            <div className="search_result">
+              <h3 onClick={() => setFoo(true)}>Search Results</h3>
+            </div>
+
+            <div style={isMovieList || localStorage.getItem("isMovieList")==="true" ? { backgroundColor: "#EEEEEE",borderRadius:"5px" } : { backgroundColor: "unset", borderRadius:"5px"}}>
+              <MovieLengthComponent
+                name="Movies"
+                length={movieLength}
+                funName={() => showMovie()}
+              />
+            </div>
+            <div style={!isMovieList || localStorage.getItem("isMovieList")==="false" ? { backgroundColor: "#EEEEEE",borderRadius:"5px" } : { backgroundColor: "unset", borderRadius:"5px"}}>
+            <MovieLengthComponent
+              name="Tv Shows"
+              length={tvShowsLength}
+              funName={() => showTvSHows()}
+            />
+            </div>
           </div>
 
-          <MovieLengthComponent
-            name="Movies"
-            length={movieLength}
-            funName={() => showMovie()}
-          />
-          <MovieLengthComponent
-            name="Tv Shows"
-            length={tvShowsLength}
-            funName={() => showTvSHows()}
-          />
-        </div>
+          <div style={{ width: "100%" }}>
+            <div
+              style={isMovieList ? { display: "block" } : { display: "none" }}
+            >
+              <SearhedMovieQuery
+                name={name}
+                queryName="searched-movie"
+                queryPath={movieQuery}
+                dispatchName={setMovieLength}
+              />
+            </div>
 
-        <div style={{ width: "100%" }}>
-          
-          <div style={isMovieList ? { display: "block" } : { display: "none" }}>
-            <SearhedMovieQuery
-              name={name}
-              queryName="searched-movie"
-              queryPath={movieQuery}
-              dispatchName={setMovieLength}
-            />
+            <div
+              style={isMovieList ? { display: "none" } : { display: "block" }}
+            >
+              <SearhedMovieQuery
+                isTv={true}
+                name={name}
+                queryName="searched-tvShows"
+                queryPath={tvShowQuery}
+                dispatchName={setTvShowLength}
+              />
+            </div>
           </div>
-            
-          <div style={isMovieList ? { display: "none" } : { display: "block" }}>
-            <SearhedMovieQuery
-              isTv={true}
-              name={name}
-              queryName="searched-tvShows"
-              queryPath={tvShowQuery}
-              dispatchName={setTvShowLength}
-            />
-          </div>
-          
         </div>
       </div>
-    </div>
     </>
   );
 };
